03038db8902ca2894d7929e2b7cee413fc2ad1c5
Pascal Martin <pascal.fb.martin@gmail.com>
2020-11-26 18:44:44 -0800

    Add minimal web UI

--- housesensor_w1.c
+++ housesensor_w1.c
@@ -82,7 +82,7 @@ static void ReadDevice (const char *id) {
                         p = value + strlen(value);
                         while (*(--p) == '0') *p = 0;
                         if (*p == '.') *p = 0; // No more fraction.
-                        housesensor_db_set ("w1", id, value, "Celsius");
+                        housesensor_db_set ("w1", id, value, "°C");
                     }
                 }
             }
